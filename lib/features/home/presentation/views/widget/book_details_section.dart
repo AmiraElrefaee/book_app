d398c8bbdb96ed16a275d72a58360afe45b186e2
amira hamed <amirahamed173@gmail.com>
2024-10-05 18:33:22 +0300

    refactor book details view

---
+++ lib/features/home/presentation/views/widget/book_details_section.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
+import 'book_rating.dart';
+import 'custom_book_image.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var widthh =MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: widthh*.2),
+          child: CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+        Text('The Jungle Book ',
+          style: Styles.textStyle30.copyWith(fontWeight: FontWeight.w600),),
+        const SizedBox(height: 6),
+        Text('Rudyard Kipling ',
+            style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic
+                ,fontWeight: FontWeight.w600,
+                color: Colors.white.withOpacity(.7)
+            )),
+        const SizedBox(height: 18,),
+        const  BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+
+      ],
+    );
+  }
+}
+
--- lib/features/home/presentation/views/widget/book_details_view_body.dart
+++ lib/features/home/presentation/views/widget/book_details_view_body.dart
@@ -1,9 +1,11 @@
 import 'package:book_application/core/utils/styles.dart';
 import 'package:book_application/features/home/presentation/views/widget/similar_books_list_view.dart';
+import 'package:book_application/features/home/presentation/views/widget/similar_books_section.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'book_action.dart';
+import 'book_details_section.dart';
 import 'book_rating.dart';
 import 'custom_book_details_app_bar.dart';
 import 'custom_book_image.dart';
@@ -13,55 +15,39 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var widthh =MediaQuery.of(context).size.width;
-    return  Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
 
-        children: [
-         const  CustomBookDetailsAppBar(),
-
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: widthh*.2),
-            child: CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          Text('The Jungle Book ',
-          style: Styles.textStyle30.copyWith(fontWeight: FontWeight.w600),),
-          const SizedBox(height: 6),
-          Text('Rudyard Kipling ',
-            style: Styles.textStyle18.copyWith(
-              fontStyle: FontStyle.italic
-                  ,fontWeight: FontWeight.w600,
-              color: Colors.white.withOpacity(.7)
-            )),
-         const SizedBox(height: 18,),
-         const  BookRating(
-           mainAxisAlignment: MainAxisAlignment.center,
-         ),
-          const SizedBox(
-            height: 37,
-          ),
-       const BookAction(),
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text('You can also like ',
-            style: Styles.textStyle14.copyWith(
-              fontWeight: FontWeight.w600
-            ),),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          SimilarBooksListView(),
-          const SizedBox(height: 40,),
-        ],
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child:Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 30),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const  CustomBookDetailsAppBar(),
+                  BookDetailsSection(),
+                  const SizedBox(
+                    height: 37,
+                  ),
+                  const BookAction(),
+                  Flexible(
+                   fit: FlexFit.loose,
+                    child: const SizedBox(
+                      height: 40,
+                    ),
+                  ),
+                  SimilarBooksSection(),
+                  const SizedBox(height: 40,),
+                ],
+              ),
+            ),
+          ) ,
+        )
+      ],
     );
+
+
   }
 }
