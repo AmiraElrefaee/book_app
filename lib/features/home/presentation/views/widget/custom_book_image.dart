fc267cb63448078380eece857dbe4b3c92304240
amira hamed <amirahamed173@gmail.com>
2024-10-12 22:49:25 +0300

    cached network image

--- lib/features/home/presentation/views/widget/custom_book_image.dart
+++ lib/features/home/presentation/views/widget/custom_book_image.dart
@@ -1,4 +1,5 @@
 import 'package:book_application/core/utils/asset_data.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -9,16 +10,23 @@ class CustomBookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.6 / 4,
-      child: Container(
+      child:CachedNetworkImage(imageUrl: imgURL,
+      imageBuilder: (context, imageProvider)=> Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.red,
-          image:  DecorationImage(
-              image: NetworkImage(imgURL),
-              fit: BoxFit.fill)
-              // AssetImage(AssetsData.tset), ,
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.red,
+            image:  DecorationImage(
+                image: NetworkImage(imgURL),
+                fit: BoxFit.fill)
+          // AssetImage(AssetsData.tset), ,
         ),
       ),
+        progressIndicatorBuilder: (context, url, downloadProgress) =>
+            Center(child: CircularProgressIndicator(value: downloadProgress.progress)),
+        errorWidget: (context, url, error) => Icon(Icons.error,
+        size: 80,),
+      ),
+
     );
   }
 }
